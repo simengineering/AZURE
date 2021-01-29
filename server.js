9809443a7242b9314ad1971eab32cf613eae3ad9
Matthew Hopper <76822419+simengineering@users.noreply.github.com>
2021-01-29 11:55:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.json());
-app.use(express.static("."));
+app.use(express.static("/"));
 
 app.get('/', function(req, res)  {
     //setting middleware
